729b5d5d152edda7bf070772fa1593f8fd47eeb4
João Pedro Magrini <reccojp65@gmail.com>
2023-05-19 13:52:44 -0300

    commit

---
+++ JavaS.js
@@ -0,0 +1,16 @@
+// var valor = confirm("Vamos juntos aprender a programar em JS?");
+// if (valor == true ) {
+//     alert("parabens, vamos lá!!!");
+// } else {
+//     alert("Opção errada, aperte F5 para recarregar a página");
+// }
+
+var email = prompt("Digite seu e-mail", "");
+if {
+
+    alert("O e-mail " + email + " foi coletado com sucesso");
+} 
+else {
+        alert("O e-mail " + email + " Não foi coletado com sucesso");
+    }
+
